b8e1954bcab07f16a1789411e7e226c914bba614
rehankhimani <muhammadrehanabdulqadir@gmail.com>
2026-01-15 01:54:49 +0500

    bugs resolve

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
 const app = require("./src/app");
 
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
