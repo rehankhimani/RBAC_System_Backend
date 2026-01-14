e6af3670fae35c4df7a57bb771a90716a8fe6249
rehankhimani <muhammadrehanabdulqadir@gmail.com>
2026-01-15 00:22:14 +0500

    backend update

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../Services/authservice");
+const authController = require("../controllers/authController");
 
 router.post("/login", authController.login);
 
