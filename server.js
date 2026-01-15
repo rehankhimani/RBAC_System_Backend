61c4f6cb6f5be01b200d37cc1958871cbfd2d5b8
rehankhimani <muhammadrehanabdulqadir@gmail.com>
2026-01-15 13:12:08 +0500

    backned deploye issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,14 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 module.exports = db.promise();
